8fc4ff39171839d43100604c69966c62a2361318
Mahdi <mahdi.mousavi0011@gmail.com>
2025-03-27 16:23:50 +0100

    button with animation added.

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@
 import "./App.scss";
 import Button from "./components/Button/Button";
 import Button2 from "./components/Button2/Button2";
+import Button3 from "./components/button3/button3";
 import Pulse from "./components/Puls/Pulse";
 import Rotation from "./components/Rotation/Rotation";
 import ScalingBox from "./components/scalingbox/scalingbox";
@@ -23,6 +24,7 @@ function App() {
 			<hr />
       <Button2 bgColor={"skyblue"} />
       <hr />
+      <Button3 hasIcon={true}/>
 			
     </>
   );
---
+++ src/components/button3/button3.tsx
@@ -0,0 +1,12 @@
+import "./button3.scss"
+type Button3Props = {
+  hasIcon?: boolean;
+}
+
+export default function Button3({hasIcon = false} :Button3Props) {
+  return (
+    <div>
+  <button className={`Button3 ${hasIcon ? "hasIcon" : ""}`}>Click me!</button>
+    </div>
+  )
+}
